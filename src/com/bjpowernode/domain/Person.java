d9be845e7f6ede1a8dd28b1856d96d8fc236451f
KANG <KANG@qq.com>
2020-04-13 21:22:28 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.domain
+ * Description:
+ *
+ * @Date:2020/4/13 21:12
+ * @Author:liujikang@bjpowernode.com
+ */
+public class Person {
+    private String id;
+}
